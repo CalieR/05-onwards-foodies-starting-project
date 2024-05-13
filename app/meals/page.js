a9d2ec701242753e0e8abd6ced6e5c70d57a9265
CalieR <calielrushton@gmail.com>
2024-05-13 18:22:49 +0100

    Set up db and handled loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,16 @@
 import Link from "next/link";
-import MealsGrid from "@/components/meals/mealsGrid";
-import classes from "./page.module.css";
+import { Suspense } from "react";
 
-export default function Meals() {
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/mealsGrid";
+import { getMeals } from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +24,11 @@ export default function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
