1ee8d414db127fcbeeb5b9b772b800cf74a3dfb4
CalieR <calielrushton@gmail.com>
2024-05-14 11:55:35 +0100

    handled loading an individual meal

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Please try again later</p>
+    </main>
+  );
+}
+
+// can use the error prop to output more specific message
